cd5cbf68a1fb5057106d7228e76655390e7c62a8
Muhammad <usamakhanmuhammad88@gmail.com>
2024-08-16 01:08:46 -0700

    Update Code

--- src/components/Nav/MainSection.tsx
+++ src/components/Nav/MainSection.tsx
@@ -8,11 +8,11 @@ const MainSection = () => {
             </div>
             <div>
                 <ul className='flex flex-col gap-6 sm:gap-9 sm:flex-row justify-left items-left sm:border rounded-full px-5 py-2 sm:bg-gray-900'>
-                    <li className='text-xl'>All</li>
-                    <li className='text-xl'>Academy</li>
-                    <li className='text-xl'>Company</li>
-                    <li className='text-xl'>Product</li>
-                    <li className='text-xl'> Regulation</li>
+                    <li className='text-xl text-white'>All</li>
+                    <li className='text-xl text-white'>Academy</li>
+                    <li className='text-xl text-white'>Company</li>
+                    <li className='text-xl text-white'>Product</li>
+                    <li className='text-xl text-white'> Regulation</li>
                 </ul>
             </div>
         </div>
